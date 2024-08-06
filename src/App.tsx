a5cbdab237fc7dfbe9c3cafc8b1241670ca12a74
Raj-Shopify-Devloper <raj.s.webdevloper@gmail.com>
2024-08-06 16:13:06 +0530

    Update

--- src/App.tsx
+++ src/App.tsx
@@ -5,9 +5,7 @@ import Skill from './my-project/skill'
 import Projects from './my-project/project'
 import Contect from './my-project/contect'
 import Footer from './my-project/footer'
-import useJQueryEffects from './my-project/useffects'
 const App: React.FC = () => {
-  useJQueryEffects();
   return (
     <>
       <Index />
